a5be9015f9801b0e4747a7f68b0f379eefed4c72
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-05-19 08:32:14 -0300

    styles globals

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
+import { Container } from "./styles.js";
 
 function App() {
   const [list, setList] = useState([{ id: uuid(), task: "Nada" }]);
+  const [inputTask, setInputTask] = useState("");
 
   function changed(event) {
-    console.log(event.target.value);
-    setList([{ id: uuid(), task: event.target.value }]);
-
-    console.log(list);
+    setInputTask(event.target.value);
   }
 
   function buttonActivated() {
-    console.log("button Activated");
+    setList([...list, { id: uuid(), task: inputTask }]);
   }
 
   return (
-    <div>
-      <div>
-        <input onChange={changed} placeholder="O que tenho que fazer..." />
-        <button onClick={buttonActivated}>Adcionar</button>
+    <Container>
+      <input onChange={changed} placeholder="O que tenho que fazer..." />
+      <button onClick={buttonActivated}>Adcionar</button>
 
-        <ul>
-          {list.map((item) => (
-            <li key={item.id}>{item.task}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.task}</li>
+        ))}
+      </ul>
+    </Container>
   );
 }
 
